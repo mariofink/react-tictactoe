0d9f0a2120caee38ab277884015fd7c7b04324ba
Mario Fink <mario.fink@goodyear.com>
2019-01-11 13:35:15 +0100

    add redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
 import "./index.css";
 import Game from "./Game";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById("root")
+);
