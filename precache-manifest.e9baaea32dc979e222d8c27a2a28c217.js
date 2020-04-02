d7f566ce1b0db03847b20e58724334230808095b
Mario Fink <mario.fink@goodyear.com>
2020-04-02 08:45:08 +0200

    Updates

---
+++ precache-manifest.e9baaea32dc979e222d8c27a2a28c217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95adb4a9c10ff11abce991be6bd015c8",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "577970372500735ea8a7",
+    "url": "/react-tictactoe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "694e13f8813f633fa1af",
+    "url": "/react-tictactoe/static/js/2.4eb12bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.4eb12bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577970372500735ea8a7",
+    "url": "/react-tictactoe/static/js/main.36c9117f.chunk.js"
+  },
+  {
+    "revision": "dd84082bd0ccc7662c3d",
+    "url": "/react-tictactoe/static/js/runtime-main.e92edc2e.js"
+  }
+]);
